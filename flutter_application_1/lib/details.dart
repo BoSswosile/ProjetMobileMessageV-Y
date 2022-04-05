7284f7058a2f67d627d5caf03de7ca9593f358e9
BoSswosile <yohan.moren@gmail.com>
2022-04-05 16:22:47 +0200

    username/name

--- flutter_application_1/lib/details.dart
+++ flutter_application_1/lib/details.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/functions/FirebaseHelper.dart';
 import 'package:flutter_application_1/globalVarialble.dart';
+import 'package:flutter_application_1/signup.dart';
 
 import 'model/contact_user.dart';
 import 'model/users.dart';
@@ -13,13 +15,105 @@ class detailsUser extends StatefulWidget {
 }
 
 class detailsUserState extends State<detailsUser> {
+  late String username;
+  late String name;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: bodyPage(),
     );
   }
+  Widget _buildPopupDialogsurname(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Voulez vous changer de nom'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextField(
+            onChanged: (value){
+              setState(() {
+                username = value;
+              });
+            },
+            decoration: new InputDecoration.collapsed(
+                border: OutlineInputBorder(),
+                hintText: 'Nom'
+            ),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            Map<String, dynamic> map = {
+              "NOM": username,
+            };
+              FirebaseHelper().updateUser(userConnect.id, map);
+            Navigator.of(context).pop();
+            setState(() {
+              userConnect.nom = username;
+            });
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: const Text('Update'),
+        ),
+        FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: const Text('Close'),
+        ),
+      ],
+    );
+  }
 
+  Widget _buildPopupDialogname(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Voulez vous changer de prénom'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextField(
+            onChanged: (value){
+              setState(() {
+                name = value;
+              });
+            },
+            decoration: new InputDecoration.collapsed(
+                border: OutlineInputBorder(),
+                hintText: 'Prénom'
+            ),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            Map<String, dynamic> map = {
+              "PRENOM": name,
+            };
+            FirebaseHelper().updateUser(userConnect.id, map);
+            Navigator.of(context).pop();
+            setState(() {
+              userConnect.prenom = name;
+            });
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: const Text('Update'),
+        ),
+        FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: const Text('Close'),
+        ),
+      ],
+    );
+  }
 
   Widget bodyPage() {
     return Center(child: Column(
@@ -36,7 +130,18 @@ class detailsUserState extends State<detailsUser> {
               )
           ),
         ),
-        Text("${userConnect.prenom} ${userConnect.nom}", style: TextStyle(fontSize: 20),),
+        GestureDetector(
+          onTap: () {
+            showDialog(context: context, builder: (BuildContext context) => _buildPopupDialogname(context),);
+          },
+        child: Text("${userConnect.prenom}", style: TextStyle(fontSize: 20),),
+        ),
+        GestureDetector(
+          onTap: () {
+            showDialog(context: context, builder: (BuildContext context) => _buildPopupDialogsurname(context),);
+            },
+          child: Text("${userConnect.nom}", style: TextStyle(fontSize: 20),),
+        ),
         SizedBox(height: 20,),
         Text(userConnect.email, style: TextStyle(),),
       ],
